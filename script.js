c86e6b55df9f6b6965cb09b41c52bc116aa69bde
Brandon-Lee Newman <bdupreez017@gmail.com>
2025-01-19 22:58:10 +0200

    JSL-01 commit

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ function validateSyntax() {
     let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
+    let pattern = /^pet_[a-zA-Z0-9]+$/;
+
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        if (pattern.test(input)) {
+            result = 'valid syntax!' + '\u{2705}';
+        } else {
+            result = 'invalid syntax!' + '\u{274C}';
+        }
 
             document.getElementById('result').innerText = result;
 }
